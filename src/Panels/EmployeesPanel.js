f17cfae74cecbb3ec37871ce612941f7f02c64e3
Natan Ross <nross9@myseneca.ca>
2018-02-14 08:28:55 -0500

    Folder Structure added with templated pages inside

---
+++ src/Panels/EmployeesPanel.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import whatever else you like here
+
+// Declare your Component here
+class EmployeesPanel extends Component{
+  render(){
+    return (
+      <div>Hello World</div>
+    );
+  }
+}
+
+// export the component by name
+export default EmployeesPanel; 
---
+++ src/Panels/ProjectsPanel.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import whatever else you like here
+
+// Declare your Component here
+class ProjectsPanel extends Component{
+  render(){
+    return (
+      <div>Hello World</div>
+    );
+  }
+}
+
+// export the component by name
+export default ProjectsPanel; 
