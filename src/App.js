5f63335a059b33fa12a7774680e1eb01e0826004
Natan Ross <nross9@myseneca.ca>
2018-02-14 08:37:20 -0500

    SideBar Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import NavBar from './DisplayItems/NavBar'
+import NavBar from './DisplayItems/NavBar';
+import SideBar from './DisplayItems/SideBar';
+
 class App extends Component {
   render() {
     return (
@@ -7,16 +9,7 @@ class App extends Component {
         <NavBar/>
         <div className="container-fluid">
           <div className="row">
-            <div className="col-sm-3 col-md-2  sidebar">
-              <ul className="nav nav-sidebar">
-                <li className="active"><a href="/">Overview <span className="sr-only">(current)</span></a></li>
-              </ul>
-              <ul className="nav nav-sidebar">
-                <li><a href="/projects">Projects</a></li>
-                <li><a href="/teams">Teams</a></li>
-                <li><a href="/employees">Employees</a></li>
-              </ul>
-            </div>
+          <SideBar/>
             <div className=" col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
               <h1 className="page-header">Overview</h1>
               <div className="row">
--- src/DisplayItems/SideBar.js
+++ src/DisplayItems/SideBar.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 // import whatever else you like here
 
 // Declare your Component here
-class SideBar extends Component{
-  render(){
+class SideBar extends Component {
+  render() {
     return (
-      <div>Hello World</div>
+      <div>
+        <div className="col-sm-3 col-md-2  sidebar">
+          <ul className="nav nav-sidebar">
+            <li className="active"><a href="/">Overview <span className="sr-only">(current)</span></a></li>
+          </ul>
+          <ul className="nav nav-sidebar">
+            <li><a href="/projects">Projects</a></li>
+            <li><a href="/teams">Teams</a></li>
+            <li><a href="/employees">Employees</a></li>
+          </ul>
+        </div>
+      </div>
     );
   }
 }
