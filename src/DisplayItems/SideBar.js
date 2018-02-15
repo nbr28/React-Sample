7531aa0950515f69f57b438deadbac912dffc6e7
Natan Ross <nross9@myseneca.ca>
2018-02-15 12:39:20 -0500

    Side bar updated to have active field

--- src/DisplayItems/SideBar.js
+++ src/DisplayItems/SideBar.js
@@ -8,12 +8,12 @@ class SideBar extends Component {
       <div>
         <div className="col-sm-3 col-md-2  sidebar">
           <ul className="nav nav-sidebar">
-            <li className="active"><a href="/">Overview <span className="sr-only">(current)</span></a></li>
+            <li className={(this.props.highlight === 'Overview' ? 'active' : '')}><a href="/">Overview <span className="sr-only">(current)</span></a></li>
           </ul>
           <ul className="nav nav-sidebar">
-            <li><a href="/projects">Projects</a></li>
-            <li><a href="/teams">Teams</a></li>
-            <li><a href="/employees">Employees</a></li>
+            <li className={(this.props.highlight === 'Projects' ? 'active' : '')}><a href="/projects">Projects</a></li>
+            <li className={(this.props.highlight === 'Teams' ? 'active' : '')}><a href="/teams">Teams</a></li>
+            <li className={(this.props.highlight === 'Employees' ? 'active' : '')}><a href="/employees">Employees</a></li>
           </ul>
         </div>
       </div>
